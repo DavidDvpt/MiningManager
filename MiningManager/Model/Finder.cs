f074ca1d18ea35409a820f6e5f608b67b6ec2e22
David_Dev <david.mosca69@gmail.com>
2019-05-07 08:34:17 +0200

    Update Finder.cs
    Suppression d'annotation inutile pour le modèle Finder

--- MiningManager/Model/Finder.cs
+++ MiningManager/Model/Finder.cs
@@ -6,10 +6,8 @@ namespace MiningManager.Model
     [Table("Finder")]
     public class Finder : Tool
     {
-        [Range(0, 1000)]
         public decimal Depth { get; set; }
 
-        [Range(0, 60)]
         public decimal Range { get; set; }
 
         public short BasePecSearch { get; set; }
