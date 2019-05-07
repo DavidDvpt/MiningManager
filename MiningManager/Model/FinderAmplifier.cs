10227d04e3389366256f41ae4664c05f1a965671
David_Dev <david.mosca69@gmail.com>
2019-05-07 08:35:21 +0200

    Update FinderAmplifier.cs
    Suppression d'annotation inutile pour le modèle FinderAmplifier

--- MiningManager/Model/FinderAmplifier.cs
+++ MiningManager/Model/FinderAmplifier.cs
@@ -6,7 +6,6 @@ namespace MiningManager.Model
     [Table("FinderAmplifier")]
     public class FinderAmplifier : Unstackable
     {
-        [Range(0, 100)]
         public decimal Coefficient { get; set; }
     }
 }
